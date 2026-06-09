c8da81910ce553e8983db8f3ecab9bbf61345379
Asher Azriel Ginting <asherginting3@gmail.com>
2026-06-09 17:26:04 +0700

    chore: update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,50 +43,11 @@ export default function Hero() {
         </span>
       </h1>
       <div className="my-4 sm:my-5 text-base sm:text-lg md:text-[20px] font-normal flex items-center gap-1.5">
-        <span>Get in touch</span>
+        <span>Keep learning</span>
         <span className="select-none pointer-events-none" aria-hidden="true">
-          👉
+          →
         </span>
-        <motion.a
-          href="mailto:asherginting3@gmail.com"
-          className="relative inline-block px-2"
-          initial="rest"
-          whileHover="hover"
-          whileTap="hover"
-          animate="rest"
-        >
-          <motion.span
-            variants={{
-              rest: { scaleY: 0 },
-              hover: { scaleY: 1 },
-            }}
-            transition={{
-              duration: 0.25,
-              ease: [0.22, 1, 0.36, 1],
-            }}
-            className="absolute inset-0 origin-bottom bg-(--color-accent) z-0"
-          />
-          <motion.span
-            variants={{
-              rest: {
-                color: "var(--color-text-primary)",
-              },
-              hover: {
-                color: "#ffffff",
-              },
-            }}
-            transition={{ duration: 0.2 }}
-            className="
-              relative z-10
-              underline
-              decoration-underline
-              decoration-2
-              underline-offset-6
-            "
-          >
-            asherginting3@gmail.com
-          </motion.span>
-        </motion.a>
+        <span>Keep growing.</span>
       </div>
     </section>
   );
